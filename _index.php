a0eb962e9c916171a2b9c37dc5d2dfadfa7cfdb1
yobafromstarwars <82865142+yobafromstarvvars@users.noreply.github.com>
2023-01-16 12:23:09 +0300

    Create _index.php

---
+++ _index.php
@@ -0,0 +1,13 @@
+<main>
+  <h1>Kungurtsev ISiP-35; 2023; Second term</h1>
+  <h2>Content:</h2>
+  <?php 
+    // Generate content list. $chapters is in config.php
+    foreach($chapters as &$chapter) {
+      echo '<li>'.$chapter.'</li>';
+      foreach($chapter as &$subchapter){
+        echo '<li>'.$subchapter.'</li>'; 
+      }
+    }
+  ?>
+</main>
